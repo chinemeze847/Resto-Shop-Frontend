4f331abac5f0cd14d32aaa7c78d74ce2b144a586
chinemeze847 <thankgodchinemeze@gmail.com>
2024-08-24 15:50:23 +0100

    Added footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Menu from './pages/Menu/Menu';
+import Home from './pages/Home/Home';
+import Footer from './components/Footer/Footer';
 
 
 const App: React.FC = () => {
